63f5467cdec7bb8ddde1eee2f3d5a2976d67522b
MohammadJavad2007 <114733800+MohammadJavad2007@users.noreply.github.com>
2023-08-23 02:21:27 +0430

    update

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,42 @@
+// import 'dart:convert';
 import 'dart:html';
+import 'package:dio/dio.dart';
+
+final InputElement toDoInput = querySelector('#to-do-input') as InputElement;
+final UListElement toDoList = querySelector('#to-do-list') as UListElement;
+
+final dio = Dio();
+void get() async {
+  Response response;
+  response =
+      await dio.get('http://localhost:3030/', options: Options(method: 'GET'));
+  print(response.data.toString());
+  // querySelector('#id')?.text = response.data[0]['Name'].toString();
+  // querySelector('#name')?.text = response.data[0]['Age'].toString();
+  final newToDo = LIElement()..text = response.data[0]['Name'].toString();
+  toDoInput.value = '';
+  toDoList.children.add(newToDo);
+}
+
+void post() async {
+  Response response;
+  response = await dio.post('http://localhost:3030/post',
+      data: [
+        {"Name": toDoInput.value, "Age": toDoInput.value}
+      ],
+      options: Options(
+        method: 'POST',
+      ));
+  print(response.data.toString());
+  // querySelector('#id-post')?.text = response.data[0]['Name'].toString();
+  // querySelector('#name-post')?.text = response.data[0]['Age'].toString();
+}
 
 void main() {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+  toDoInput.onChange.listen(addToDoItem);
+}
+
+void addToDoItem(Event e) {
+  post();
+  get();
 }
