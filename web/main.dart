ae909980d118e954ea2795c166aa55b7a007dd2f
MohammadJavad2007 <114733800+MohammadJavad2007@users.noreply.github.com>
2023-08-24 00:54:25 +0430

    Update main.dart

--- web/main.dart
+++ web/main.dart
@@ -11,25 +11,61 @@ void get() async {
   Response response;
   response =
       await dio.get('http://localhost:3030/', options: Options(method: 'GET'));
-  print(response.data.toString());
+  print(response.data.length.toString());
+  int i = 0;
+  for (i; i < response.data.length; i++) {
+    final newToDo = LIElement()
+      ..text = response.data[i]['id'].toString() +
+          ' ' +
+          response.data[i]['Name'].toString();
+
+    newToDo.onClick.listen((_) async {
+      print(newToDo.innerText.split(' ')[0]);
+      response = await dio.delete(
+          'http://localhost:3030/p/${newToDo.innerText.split(' ')[0]}',
+          data: [
+            {"id": int.parse(newToDo.innerText.split(' ')[0])}
+          ],
+          options: Options(
+            method: 'DELETE',
+          ));
+      newToDo.remove();
+    });
+    toDoList.children.add(newToDo);
+    // print('ali');
+  }
   // querySelector('#id')?.text = response.data[0]['Name'].toString();
   // querySelector('#name')?.text = response.data[0]['Age'].toString();
-  final newToDo = LIElement()..text = response.data[0]['Name'].toString();
-  newToDo.onClick.listen((_) => newToDo.remove());
-  toDoInput.value = '';
-  toDoList.children.add(newToDo);
 }
 
 void post() async {
   Response response;
-  response = await dio.post('http://localhost:3030/post',
+  response = await dio.post('http://localhost:3030/p',
       data: [
-        {"Name": toDoInput.value, "Age": toDoInput.value}
+        {"Name": toDoInput.value, "Age": toDoInput.value, "id": null}
       ],
       options: Options(
         method: 'POST',
       ));
   print(response.data.toString());
+  final newToDo = LIElement()
+    ..text = response.data[0]['id'].toString() +
+        ' ' +
+        response.data[0]['Name'].toString();
+  newToDo.onClick.listen((_) async {
+    print(newToDo.innerText.split(' ')[0]);
+    response = await dio.delete(
+        'http://localhost:3030/p/${newToDo.innerText.split(' ')[0]}',
+        data: [
+          {"id": int.parse(newToDo.innerText.split(' ')[0])}
+        ],
+        options: Options(
+          method: 'DELETE',
+        ));
+    newToDo.remove();
+  });
+  toDoInput.value = '';
+  toDoList.children.add(newToDo);
   // querySelector('#id-post')?.text = response.data[0]['Name'].toString();
   // querySelector('#name-post')?.text = response.data[0]['Age'].toString();
 }
@@ -37,9 +73,9 @@ void post() async {
 void main() {
   toDoInput.onChange.listen(addToDoItem);
   deleteAll.onClick.listen((_) => toDoList.children.clear());
+  get();
 }
 
 void addToDoItem(Event e) {
   post();
-  get();
 }
